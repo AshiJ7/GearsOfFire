6dff5098546fdcf2b1b3f17802467f5402b50c32
AshiJ7 <56090408+AshiJ7@users.noreply.github.com>
2021-12-27 12:47:34 -0500

    Update Hardware.java

--- Hardware.java
+++ Hardware.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.service.autofill.DateValueSanitizer;
+
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -16,13 +18,17 @@ public class Hardware {
     public DcMotor lf;
     public DcMotor lb;
     public DcMotor intake;
-    public Servo sCarsl;
+    public Servo leftduckwheel;
+    public Servo rightduckwheel;
     public Servo sInt;
+    public DcMotor lift;
+    public DcMotor extension;
+    public DcMotor transfer;
 
     //public ModernRoboticsI2cGyro gyro;
     private static Hardware myInstance = null;
 
-    public double maxSpeed = 0.8;
+    public double maxSpeed = 1;
 
     public static Hardware getInstance() {
         if (myInstance == null) {
@@ -92,10 +98,18 @@ public class Hardware {
 
         //servo for carousel
         try {
-            sCarsl = hwMap.get(Servo.class, "servo carousel");
+            leftduckwheel = hwMap.get(Servo.class, "left motor carousel");
         }
-        catch (Exception p_exception) {
-            sCarsl = null;
+        catch(Exception p_exception) {
+            leftduckwheel = null;
+        }
+
+        //servo for carousel
+        try {
+            rightduckwheel = hwMap.get(Servo.class, "right motor carousel");
+        }
+        catch(Exception p_exception) {
+            rightduckwheel = null;
         }
 
         //motor for intake
@@ -111,44 +125,100 @@ public class Hardware {
 
         //servo for intake
         try {
-            sInt = hwMap.get(Servo.class, "servo intake");
-            sInt.setDirection(Servo.Direction.REVERSE);
+         sInt = hwMap.get(Servo.class, "servo intake");
+         sInt.setDirection(Servo.Direction.REVERSE);
+         }
+         catch(Exception p_exception) {
+         sInt = null;
+         }
+
+        //motor for lift
+        try {
+            lift = hwMap.get(DcMotor.class, "lift motor");
+            lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            lift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            lift.setPower(0);
         }
         catch(Exception p_exception) {
-            sInt = null;
+            lift = null;
+        }
+
+        //motor for extension
+        try {
+            extension = hwMap.get(DcMotor.class, "extension motor");
+            extension.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            extension.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            extension.setPower(0);
+        }
+        catch (Exception p_exception) {
+            extension = null;
+        }
+
+        //motor for transferring from intake to lift
+        try {
+            transfer = hwMap.get(DcMotor.class, "transfer motor");
+            transfer.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            transfer.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            transfer.setPower(0);
+        }
+        catch (Exception p_exception) {
+            transfer = null;
         }
     }
 
-        public void setPower ( double fr, double br, double fl, double bl){
-            if (rf != null) {
-                rf.setPower(Range.clip(fr, -maxSpeed, maxSpeed));
-            }
-            if (rb != null) {
-                rb.setPower(Range.clip(br, -maxSpeed, maxSpeed));
-            }
-            if (lf != null) {
-                lf.setPower(Range.clip(fl, -maxSpeed, maxSpeed));
-            }
-            if (lb != null) {
-                lb.setPower(Range.clip(bl, -maxSpeed, maxSpeed));
-            }
+    public void setPower ( double fr, double br, double fl, double bl){
+        if (rf != null) {
+            rf.setPower(Range.clip(fr, -maxSpeed, maxSpeed));
         }
-
-        public void setsCarslPosition(double servoPos) {
-        if (sCarsl != null) {
-            sCarsl.setPosition(servoPos);
+        if (rb != null) {
+            rb.setPower(Range.clip(br, -maxSpeed, maxSpeed));
+        }
+        if (lf != null) {
+            lf.setPower(Range.clip(fl, -maxSpeed, maxSpeed));
+        }
+        if (lb != null) {
+            lb.setPower(Range.clip(bl, -maxSpeed, maxSpeed));
         }
     }
 
-        public void intakeSetPower(double power) {
+    public void leftduckPos(double dpos) {
+        if (leftduckwheel != null) {
+            leftduckwheel.setPosition(dpos);
+        }
+    }
+
+    public void rightduckPos(double dpos) {
+        if (rightduckwheel != null) {
+            rightduckwheel.setPosition(dpos);
+        }
+    }
+
+    public void intakeSetPower(double power) {
         if (intake != null) {
             intake.setPower(Range.clip(power, -maxSpeed, maxSpeed));
         }
     }
 
-        public void setsIntPosition(double pos) {
-        if(sInt != null) {
-            sInt.setPosition(pos);
+    public void liftSetLevel(double level) {
+        if (lift != null) {
+            lift.setPower(Range.clip(level, -maxSpeed, maxSpeed));
         }
     }
+
+    public void extensionPower(double epower) {
+        if (extension != null) {
+            extension.setPower(Range.clip(epower, -maxSpeed, maxSpeed));
+        }
+    }
+
+    public void transferPower(double tpower) {
+        if (transfer != null) {
+            transfer.setPower(Range.clip(tpower, -maxSpeed, maxSpeed));
+        }
+    }
+     public void setsIntPosition(double pos) {
+        if (sInt != null) {
+            sInt.setPosition(pos);
+        }
+     }
 }
